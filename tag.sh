7bc6020fa52d694fdfe3418e51d7d5709a570028
Sebastien Besson <seb.besson@gmail.com>
2019-02-07 15:00:20 +0000

    Updating all repos for IDR 0.6.2

--- tag.sh
+++ tag.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PREVIOUS=0.6.0
-UPCOMING=0.6.1
-MESSAGE="Tagging $UPCOMING: prod61 release"
+PREVIOUS=0.6.1
+UPCOMING=0.6.2
+MESSAGE="Tagging $UPCOMING: prod62 release"
 
 # Usage:
 # 0. Update the variables above!
