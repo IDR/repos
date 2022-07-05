fc1d39dcf54e7c673a505afbbc07289ec2ec1eaa
Sebastien Besson <seb.besson@gmail.com>
2022-07-05 11:50:37 +0100

    Updating all repos for IDR 0.10.7

--- tag.sh
+++ tag.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PREVIOUS=0.10.5
-UPCOMING=0.10.6
-MESSAGE="Tagging $UPCOMING: prod106 release"
+PREVIOUS=0.10.6
+UPCOMING=0.10.7
+MESSAGE="Tagging $UPCOMING: prod107 release"
 
 # Usage:
 # 0. Update the variables above!
