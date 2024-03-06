cbf445b469f55dc0a8ea97808a0c2962234a99c6
Sébastien Besson <sbesson@glencoesoftware.com>
2024-03-06 10:23:35 +0000

    Updating all repos for IDR 0.12.0

--- tag.sh
+++ tag.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-PREVIOUS=0.11.8
-UPCOMING=0.11.9
-MESSAGE="Tagging $UPCOMING: prod119 release"
+PREVIOUS=0.11.9
+UPCOMING=0.12.0
+MESSAGE="Tagging $UPCOMING: prod120 release"
 
 # Usage:
 # 0. Update the variables above!
